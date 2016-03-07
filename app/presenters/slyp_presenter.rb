e90fa5857a210a2d7d8e7ede904d36d5e538d04b
James Groeneveld <jamesgroeneveld@gmail.com>
2016-03-07 13:02:59 +1300

    Retrieving user_slyp attrs on slyp serialization

--- app/presenters/slyp_presenter.rb
+++ app/presenters/slyp_presenter.rb
@@ -1,9 +1,22 @@
 class SlypPresenter < BasePresenter
-  attr_accessor :slyp
+  attr_accessor :slyp, :user_slyp
 
-  delegate :id, :display_url, :title, :site_name, :author, to: :slyp
+  delegate :id, :duration, :display_url, :title, :site_name, :author, to: :slyp
 
-  def initialize(slyp)
+  def initialize(slyp, user_slyp)
     @slyp = slyp
+    @user_slyp = user_slyp
+  end
+
+  def archived
+    @user_slyp.archived
+  end
+
+  def favourite
+    @user_slyp.favourite
+  end
+
+  def deleted
+    @user_slyp.deleted
   end
 end
