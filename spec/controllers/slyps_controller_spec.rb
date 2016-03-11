e7719f663d3b544657ebc2d5d75aaed392dab387
James Groeneveld <jamesgroeneveld@gmail.com>
2016-03-11 22:03:53 +1100

    Shell controller specs and removing slyps controller spec

--- spec/controllers/slyps_controller_spec.rb
+++ spec/controllers/slyps_controller_spec.rb
@@ -1,39 +1,4 @@
 require "rails_helper"
-
 RSpec.describe SlypsController, type: :controller do
-  describe "#create" do
-    context "without authentication", :vcr do
-      it "responds with 401" do
-        url = "https://www.farnamstreetblog.com/2014/02/quotable-kierkegaard/"
-        post :create, url: url, format: :json
-
-        expect(response.status).to eq(401)
-      end
-    end
-
-    context "with authentication and invalid parameters", :vcr do
-      it "responds wth 422" do
-        sign_in FactoryGirl.create(:user)
-        url = "http://www.foobarbaziamafaker.co/"
-        post :create, url: url, format: :json
-
-        expect(response.status).to eq(422)
-        expect(response.content_type).to eq(Mime::JSON)
-      end
-    end
-    context "with authentication and valid parameters", :vcr do
-      it "responds with 201 and minimal slyp attrs" do
-        sign_in FactoryGirl.create(:user)
-        url = "https://www.farnamstreetblog.com/2014/02/quotable-kierkegaard/"
-        post :create, url: url, format: :json
-
-        json_response_body = JSON.parse(response.body)
-        expect(response.status).to eq(201)
-        expect(response.content_type).to eq(Mime::JSON)
-        expect(json_response_body["archived"]).not_to be_nil
-        expect(json_response_body["deleted"]).not_to be_nil
-        expect(json_response_body["favourite"]).not_to be_nil
-      end
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
