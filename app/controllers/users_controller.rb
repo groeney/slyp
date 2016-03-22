d09235346eb34cd845c0ca66abaefaa591cfd6c7
James Groeneveld <jamesgroeneveld@gmail.com>
2016-03-23 12:52:59 +1300

    Users controller for friends and all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < BaseController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
+  before_action :set_friends
+
+  def index
+    @users = User.all
+    render status: 200, json: present_collection(@users), each_serializer: UserSerializer
+  end
+
+  def friends
+    @friends = current_user.friends
+    render status: 200, json: present_collection(@friends), each_serializer: UserSerializer
+  end
+
+  private
+
+  def present(user)
+    UserPresenter.new user
+  end
+
+  def present_collection(users)
+    users.map { |user| present(user) }
+  end
+
+  def set_friends
+    @friends = current_user.friends
+  end
 end
+
--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,8 +1,17 @@
 class UserPresenter < BasePresenter
   attr_accessor :user
-  delegate :id, :first_name, :last_name, to: :user
+  delegate :id, :first_name, :last_name, :email, to: :user
 
-  def initialize(user)
+  def initialize(user, is_friend = false)
     @user = user
+    @is_friend = is_friend
+  end
+
+  def is_friend
+    @is_friend == true
+  end
+
+  def full_name
+    [user.first_name, user.last_name].reject(&:empty?).join(" ")
   end
 end
