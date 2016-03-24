ec644e85be5153878bf6a67d1feb60d91c4d27b7
James Groeneveld <jamesgroeneveld@gmail.com>
2016-03-25 12:08:05 +1300

    Redirect user to feed app after successful signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   serialization_scope :view_context
+  def after_sign_in_path_for(resource_or_scope)
+   "/feed"
+  end
 end
