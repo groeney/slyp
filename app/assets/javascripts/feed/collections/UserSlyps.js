be93b031f1763250455e7f24368ee7e02d6dfbaa
James Groeneveld <jamesgroeneveld@gmail.com>
2016-03-29 18:38:51 +1300

    Move model to front or back of collection

--- app/assets/javascripts/feed/collections/UserSlyps.js
+++ app/assets/javascripts/feed/collections/UserSlyps.js
@@ -1,7 +1,22 @@
 slypApp.Collections.UserSlyps = Backbone.Collection.extend({
   model: slypApp.Models.UserSlyp,
-  url: "/user_slyps",
+  url: '/user_slyps',
   initialize: function(){
     this.fetch();
+  },
+  moveToFront: function(model) {
+    var index = this.indexOf(model);
+    if (index > 0) {
+      this.remove(model);
+      this.add(model, {at: 0});
+    }
+  },
+  moveToBack: function(model) {
+    var index = this.indexOf(model);
+    var backIndex = this.models.length - 1;
+    if (index < backIndex) {
+      this.remove(model);
+      this.add(model, {at: backIndex});
+    }
   }
 });
--- app/assets/javascripts/feed/models/UserSlyp.js
+++ app/assets/javascripts/feed/models/UserSlyp.js
@@ -1,4 +1,13 @@
 slypApp.Models.UserSlyp = Backbone.Model.extend({
+  moveToFront: function() {
+    this.collection.moveToFront(this);
+  },
+  moveToBack: function(){
+    this.collection.moveToBack(this);
+  },
+  displayTitle: function(){
+    return this.get('title') ? this.get('title') : this.get('url')
+  },
   authorshipMeta: function(){
     if (this.get('author') && this.get('site_name')){
       return this.get('site_name') + ' | by ' + this.get('author')
