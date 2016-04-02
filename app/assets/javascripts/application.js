fe7b0d4ae962bed19c86628bd5a90d156dcc760c
James Groeneveld <jamesgroeneveld@gmail.com>
2016-04-02 18:23:01 +1300

    Fixes for deployment to heroku
    Removing second precompile command
    
    Removing unused js file
    
    Using minified libraries

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require semantic-ui/dist/semantic
-//= require underscore/underscore
+//= require semantic-ui/dist/semantic.min
+//= require underscore/underscore-min
 //= require moment/min/moment.min
 //= require toastr/toastr.min
-//= require paloma
 //= require ./hoverIntent
