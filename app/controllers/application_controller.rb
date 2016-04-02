f52845369d2257b3d4f321a96c0fdc091bbd2719
James Groeneveld <jamesgroeneveld@gmail.com>
2016-04-02 13:31:07 +1300

    Redirect after sign-in/up and spelling on copy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   serialization_scope :view_context
+
+  def after_sign_up_path_for(resource)
+    "/feed"
+  end
+
+  def after_sign_in_path_for(resource)
+    "/feed"
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,3 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
-
-  def after_sign_up_path_for(resource)
-    "feed"
-  end
-
-  def after_sign_in_path_for(resource)
-    "feed"
-  end
 end
