3ab7475e68f9ef25a615843abfc43a43c563ffe4
James Groeneveld <jamesgroeneveld@gmail.com>
2016-04-12 17:34:47 +1000

    Homepage beta request setup
    With model, controller and view all synced up.

--- db/migrate/20160411043247_create_beta_requests.rb
+++ db/migrate/20160411043247_create_beta_requests.rb
@@ -1,9 +1,11 @@
 class CreateBetaRequests < ActiveRecord::Migration
   def change
     create_table :beta_requests do |t|
-      t.string :email, null: false
       t.boolean :invited, null: false, default: false
       t.boolean :signed_up, null: false, default: false
+      t.string  :email, null: false
+      t.string  :first_name, null: false
+      t.string  :last_name
 
       t.timestamps null: false
     end
