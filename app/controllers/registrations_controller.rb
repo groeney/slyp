f47b78fc2c361fae9c695e5a9fe16a843d00ce87
James Groeneveld <jamesgroeneveld@gmail.com>
2016-04-13 22:22:48 +1000

    Allowing more params to be set on devise user model

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,4 +12,12 @@ class RegistrationsController < Devise::RegistrationsController
   def beta_request_params
     params.permit(:email)
   end
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
 end
