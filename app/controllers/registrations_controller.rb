cefee25cd974090daa147bdf54b16f839a73a177
James Groeneveld <jamesgroeneveld@gmail.com>
2016-04-14 22:29:36 +1000

    Creating standalone BetaRequest controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,3 @@
 class RegistrationsController < Devise::RegistrationsController
-  def beta_request
-    @beta_request = BetaRequest.create(beta_request_params)
-    if @beta_request.valid?
-      UserMailer.beta_request_email(@beta_request).deliver_now
-      render status: 201, json: { priority: @beta_request.id }
-    else
-      render status: 422, json: { error: @beta_request.errors.full_messages }
-    end
-  end
-  private
-  def beta_request_params
-    params.permit(:email)
-  end
+
 end
