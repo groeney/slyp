f5c92bc18fe100180e5ba80bfb4b86bbd487e095
James Groeneveld <jamesgroeneveld@gmail.com>
2016-04-17 19:36:02 +1000

    Small UI tweaks

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,4 +8,8 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
