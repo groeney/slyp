e5527b0b256bca8b189f4d487af8e95a30ba38c1
James Groeneveld <jamesgroeneveld@gmail.com>
2016-04-21 15:26:21 +1000

    Filling email input with query params if exists

--- app/assets/javascripts/home/index.js
+++ app/assets/javascripts/home/index.js
@@ -1,32 +1,33 @@
-  $('#beta-success').modal({
-    selector : { close: '.close, .actions .button'}
-  });
+$('#beta-success').modal({
+  selector : { close: '.close, .actions .button'}
+});
 
-  $('#beta-error').modal({
-    selector : { close: '.close, .actions .button'}
-  });
+$('#beta-error').modal({
+  selector : { close: '.close, .actions .button'}
+});
 
-  $('#login').click(function(){
-    $('#login-modal').modal('show');
-  });
+$('#login').click(function(){
+  $('#login-modal').modal('show');
+});
 
-  $.fn.api.settings.api = {
-    'create beta': '/beta_request'
-  };
+$.fn.api.settings.api = {
+  'create beta': '/beta_request'
+};
 
-  $('#beta-request').api({
-    action        : 'create beta',
-    method        : 'POST',
-    serializeForm : true,
-    onSuccess     : function(response, element, xhr) {
-      $('#beta-success').find('.content span').text('#' + response.priority);
-      $('#beta-success').modal('show');
-    },
-    onError       : function(errorMessage, element, xhr) {
-      $('#beta-error').modal('show');
-    },
-    onFailure       : function(response, element) {
-      $('#beta-error').modal('show');
-    }
-  });
+$('#beta-request').api({
+  action        : 'create beta',
+  method        : 'POST',
+  serializeForm : true,
+  onSuccess     : function(response, element, xhr) {
+    $('#beta-success').find('.content span').text('#' + response.priority);
+    $('#beta-success').modal('show');
+  },
+  onError       : function(errorMessage, element, xhr) {
+    $('#beta-error').modal('show');
+  },
+  onFailure       : function(response, element) {
+    $('#beta-error').modal('show');
+  }
+});
 
+$('#beta-request [name=email]').val(getParameterByName('email'));
