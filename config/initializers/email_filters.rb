4ec789083e34ab976f022640cc33222f93d472f1
James Groeneveld <jamesgroeneveld@gmail.com>
2016-05-02 22:00:09 +1000

    Block emails to @example domains

---
+++ config/initializers/email_filters.rb
@@ -0,0 +1,13 @@
+class EmailAddressFilter
+  def self.delivering_email(message)
+    message.perform_deliveries = false
+
+    # your checks here; return if @abc.com, etc.. is matched
+    return unless message.to.join("").match(/@example.com/).nil?
+
+    # otherwise, the email should be sent
+    message.perform_deliveries = true
+  end
+end
+
+ActionMailer::Base.register_interceptor(EmailAddressFilter)
