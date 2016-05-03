f795b1cbe12e0229753e17a402c06fe3f4ba3103
James Groeneveld <jamesgroeneveld@gmail.com>
2016-05-03 17:40:05 +1000

    Forcing invitee to update first/last_name on edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :omniauth_callbacks => "users/omniauth_callbacks",
-    :registrations => "registrations"
+    :registrations => "registrations",
+    :invitations => "invitations"
   }
 
   root to: "home#index"
