2c230ccd0cf48b29dcec8fc2537ca9dd571fa749
James Groeneveld <jamesgroeneveld@gmail.com>
2016-05-04 23:50:57 +1000

    Better invitation email logic

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,9 +1,11 @@
 class CustomDeviseMailer < Devise::Mailer
+  default from: "James Groeneveld <jamesgroeneveld@gmail.com>"
   protected
 
   def subject_for(key)
     return super unless key.to_s == "invitation_instructions"
     invited_by = resource.invited_by.try(:full_name) || "Someone"
+    resource.update_attribute(:invitations_count, resource.invitations_count + 1)
     I18n.t("devise.mailer.invitation_instructions.subject",:invited_by => invited_by)
   end
 end
