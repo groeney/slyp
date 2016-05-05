2500c0d28ccfe391f577a96f551301d1c137ac4e
James Groeneveld <jamesgroeneveld@gmail.com>
2016-05-05 13:48:19 +1000

    Indicate pending user on search dropdown

--- app/presenters/user_search_presenter.rb
+++ app/presenters/user_search_presenter.rb
@@ -1,8 +1,12 @@
 class UserSearchPresenter < BasePresenter
   attr_accessor :user
-  delegate :display_name, :email, to: :user
+  delegate :email, to: :user
 
   def initialize(user)
     @user = user
+  end
+
+  def display_name
+    user.invitation_pending? ? user.email + " (pending)" : [user.first_name, user.last_name].reject(&:empty?).join(" ")
   end
 end
