af1f8d544a7fae8448ca9c0e187de36ffaf54439
James Groeneveld <jamesgroeneveld@gmail.com>
2016-05-06 18:26:15 +1000

    Default to remember user for 1 year

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :omniauth_callbacks => "users/omniauth_callbacks",
     :registrations => "registrations",
-    :invitations => "invitations"
+    :invitations => "invitations",
+    :sessions => "sessions"
   }
 
   root to: "home#index"
