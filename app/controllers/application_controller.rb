bac83679ff9164d74eee26c03bd3e13328aadd6f
James Groeneveld <jamesgroeneveld@gmail.com>
2016-05-24 23:26:38 +1000

    Adding last_seen_at to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   serialization_scope :view_context
 
+  before_action :set_last_seen_at
   def after_sign_up_path_for(_resource)
     "/feed"
   end
 
   def after_sign_in_path_for(_resource)
     "/feed"
+  end
+
+  private
+
+  def set_last_seen_at
+    return unless current_user && (session[:last_seen_at].nil? ||
+                                   session[:last_seen_at] < 15.minutes.ago)
+    current_user.update_attribute(:last_seen_at, Time.now)
+    session[:last_seen_at] = Time.now
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503011701) do
+ActiveRecord::Schema.define(version: 20160524131137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,6 +129,7 @@ ActiveRecord::Schema.define(version: 20160503011701) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
+    t.datetime "last_seen_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
