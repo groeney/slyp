3d15ff51e26c3ca4146279fe128399376176204f
James Groeneveld <jamesgroeneveld@gmail.com>
2016-05-26 15:24:37 +1000

    Helper functions on user model and returning user id with search

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   after_create :send_welcome_email
   after_create :remove_from_waitlist
+  after_create :befriend_inviter
 
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
@@ -15,6 +16,10 @@ class User < ActiveRecord::Base
   has_many :friendships
   has_many :friends, through: :friendships
   scope :all_except, ->(user) { where.not(id: user) }
+
+  def active_user_slyps
+    user_slyps.where(archived: false, deleted: false)
+  end
 
   def invitation_pending?
     !invitation_token.nil? && invitation_accepted_at.nil?
@@ -32,12 +37,28 @@ class User < ActiveRecord::Base
     BetaRequest.find_by(email: email).try(:update, signed_up: true)
   end
 
+  def befriend_inviter
+    befriend(invited_by_id)
+  end
+
   def display_name
     full_name.empty? ? email : full_name
   end
 
+  def find_mutual_user_slyps(friend_id)
+    return active_user_slyps if friend_id == id
+    user_slyp_friend_map = Hash[user_slyps.map { |el| [el.id, el.friend_ids] }]
+    user_slyp_ids = user_slyp_friend_map
+                    .select{ |key, ids| ids.include? friend_id }.keys
+    UserSlyp.where(id: user_slyp_ids)
+  end
+
+  def befriend(friend_id)
+    Friendship.create(user_id: id, friend_id: friend_id) unless friend_id.nil?
+  end
+
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    self.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.first_name = auth.info.name # assuming the user model has a name
