60a198c81837c5cd8b8b8cbbed0fb8755323c5ea
James Groeneveld <jamesgroeneveld@gmail.com>
2016-06-10 13:37:51 +1000

    Hotfix

--- app/presenters/primary_user_presenter.rb
+++ app/presenters/primary_user_presenter.rb
@@ -1,7 +1,7 @@
 class PrimaryUserPresenter < BasePresenter
   attr_accessor :user
   delegate :id, :first_name, :last_name, :email, :display_name, :notify_reslyp,
-           :notify_activity, :cc_me_on_email_reslyp, :weekly_summary,
+           :notify_activity, :cc_on_reslyp_email_contact, :weekly_summary,
            :searchable, to: :user
 
   def initialize(user)
--- app/serializers/primary_user_serializer.rb
+++ app/serializers/primary_user_serializer.rb
@@ -1,5 +1,5 @@
 class PrimaryUserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :email, :full_name, :display_name,
              :friends, :notify_reslyp, :notify_activity, :weekly_summary,
-             :searchable, :cc_me_on_email_reslyp
+             :searchable, :cc_on_reslyp_email_contact
 end
