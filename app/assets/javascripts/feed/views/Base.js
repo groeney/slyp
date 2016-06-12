7766763d6b41d90caf0c928f4235a112ee0c5d89
James Groeneveld <jamesgroeneveld@gmail.com>
2016-06-13 01:13:25 +1000

    Reposition default toastr to top center

--- app/assets/javascripts/feed/views/Base.js
+++ app/assets/javascripts/feed/views/Base.js
@@ -2,7 +2,7 @@ slypApp.Base.CompositeView = Backbone.Marionette.CompositeView.extend({
   toastr: function(type, message, options){
     message = typeof message !== 'undefined' ? message : 'Our robots cannot perform that action right now :(';
     type = typeof type !== 'undefined' ? type : 'success'; // Default to success toastr
-    options = typeof options !== 'undefined' ? options : {};
+    options = typeof options !== 'undefined' ? options : { 'positionClass': 'toast-top-center' };
     toastr.options = options;
     toastr[type](message);
   },
