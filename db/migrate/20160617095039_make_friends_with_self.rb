88d6d328fa00417a2b0238d8ed18f4b5395a55f5
James Groeneveld <jamesgroeneveld@gmail.com>
2016-06-17 19:59:02 +1000

    Quietly make friends with self :-) :+1:

--- db/migrate/20160617095039_make_friends_with_self.rb
+++ db/migrate/20160617095039_make_friends_with_self.rb
@@ -1,7 +1,7 @@
 class MakeFriendsWithSelf < ActiveRecord::Migration
   def change
     User.find_each do |user|
-      user.befriend(user.id)
+      user.befriend(user.id, false)
     end
   end
 end
