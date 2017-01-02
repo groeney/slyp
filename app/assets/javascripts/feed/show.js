65a901c9903bc072dd751830c500d19ce602e996
James Groeneveld <jamesgroeneveld@gmail.com>
2017-01-02 13:57:55 +0000

    [PATCH] remove limited functionality message

--- app/assets/javascripts/feed/show.js
+++ app/assets/javascripts/feed/show.js
@@ -6,7 +6,6 @@ $(function() {
       controller: new slypApp.Controller
     });
   });
-  _toastr('info', 'Hi! Thank you for coming back. The functionality involved in retrieving the web content from a link has gone offline temporarily. Check in later or ask us questions below.', { 'positionClass': 'toast-top-center', 'timeOut': 30000 })
   return slypApp.start();
 });
 
