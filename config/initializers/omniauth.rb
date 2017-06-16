a473016261a6efcc3c54458ceb43dce70d5f7a47
James Groeneveld <jamesgroeneveld@gmail.com>
2017-06-16 11:24:42 +0200

    [BUG] fixing OAuth bug

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
+    client_options: {
+      site: 'https://graph.facebook.com/v2.6',
+      authorize_url: "https://www.facebook.com/v2.6/dialog/oauth"
+    }
+end
